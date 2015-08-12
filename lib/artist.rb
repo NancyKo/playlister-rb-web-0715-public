6640256c585763685489ca2ff781a6e2d90d46bc
NancyKo <konancys@gmail.com>
2015-08-12 16:00:17 -0400

    Adding artist

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,23 +2,37 @@
 require 'pry'
 class Artist
 
-attr_accessor :name, :songs
+attr_accessor :name, :songs, :genres
+@@all = []
+	
+	def self.all
+		@@all 
+	end
+
+	def self.reset_artists 
+		@@all.clear 
+	end
+
+	def self.count
+		@@all.count
+	end
+
 	def initialize
 		@name = name
 		@songs = []
+		@genres = []
+		@@all << self
 	end
 
 	def add_song(song)
-		# binding.pry
 		@songs << song
 		song.artist = self 
 	end
 
 	def genres
-		song = Song.new
-		song.genre = Genre.new
-		binding.pry
-		artist.genres song.genre
+		song = Song.new 
+		self.add_song(song)
+		@genres << song.genre
 	end
 
 
